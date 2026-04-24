f937c69810cc362264dd0b3b81387940b14377cc
ROSEY-CRYSTAL <TheRealRagdollCat@gmail.com>
2026-04-24 18:05:47 +0100

    crystals updateeee

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Eating Crystals ˖᯽ ݁˖",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#e2c0ca", 
+      secondary: "#b5cea0", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
